e2c3fc799caa9fe3aebd097addb12de5e7d379d3
Jacob Bourne <jbourne2021@fau.edu>
2021-07-23 04:48:08 -0400

    a more sensable scraper

--- scrape.py
+++ scrape.py
@@ -1,20 +1,16 @@
 import os
-import selenium
 from selenium import webdriver
 import time
 from PIL import Image
-import io
-import requests
 import urllib
-from webdriver_manager.microsoft import EdgeChromiumDriver, EdgeChromiumDriverManager
-from selenium.common.exceptions import ElementClickInterceptedException
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.keys import Keys
 
 #Install Driver
 driver = webdriver.Edge(EdgeChromiumDriverManager().install())
 driver.get('https://www.google.com/')
 search = driver.find_element_by_name('q')
-search.send_keys('Toyota 4Runner',Keys.ENTER)
+search.send_keys('Toyota Rav4',Keys.ENTER)
 
 elem = driver.find_element_by_link_text('Images')
 elem.get_attribute('href')
@@ -34,7 +30,7 @@ for i in elements:
         if src != None:
             src  = str(src)
             count+=1
-            urllib.request.urlretrieve(src, os.path.join('images/toyota/4runner','4runner'+str(count)+'.jpg'))
+            urllib.request.urlretrieve(src, os.path.join('images/toyota/rav4','rav4'+str(count)+'.jpg'))
         else:
             raise TypeError
     except TypeError:
---
+++ scraper.py
@@ -0,0 +1,55 @@
+import os
+from selenium import webdriver
+import time
+from PIL import Image
+import urllib
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.common.keys import Keys
+import json
+
+#Install Driver
+driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+
+#Parse JSON
+with open('scrape.json', 'r') as json_file:
+  car_input = json.load(json_file)
+
+
+cars = car_input["cars"]
+for make in cars:
+    for models in cars[make]:
+        model = models['model']
+
+        path = os.path.join("images",make,model)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        query = make + " " + model + " exterior"
+        driver.get('https://www.google.com/')
+        search = driver.find_element_by_name('q')
+        search.send_keys(query,Keys.ENTER)
+
+        elem = driver.find_element_by_link_text('Images')
+        elem.get_attribute('href')
+        elem.click()
+
+        value = 0
+        for i in range(500): 
+            driver.execute_script('scrollBy("+ str(value) +",+100);')
+            value += 100
+            time.sleep(2)
+
+        elements = driver.find_elements_by_xpath('//img[contains(@class,"rg_i")]')
+        count = 0
+        for i in elements:
+            src = i.get_attribute('src')
+            try:
+                if src != None:
+                    src  = str(src)
+                    count+=1
+                    urllib.request.urlretrieve(src, os.path.join(path,model+str(count)+'.jpg'))
+                else:
+                    raise TypeError
+            except TypeError:
+                pass
+
